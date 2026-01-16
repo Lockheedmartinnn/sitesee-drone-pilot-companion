01e9fc0d0e3270e0c92383ed646d828322d9c229
Yatesh Pawar <yatesh.pawar@sitesee.com.au>
2026-01-16 12:48:50 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -21,7 +21,6 @@ import ToolsLinks from './pages/ToolsLinks';
 import TrainingVideos from './pages/TrainingVideos';
 import WeatherAnalysis from './pages/WeatherAnalysis';
 import PanoramaGuideon from './pages/PanoramaGuideon';
-import PanoramaGuide from './pages/PanoramaGuide';
 import __Layout from './Layout.jsx';
 
 
@@ -49,7 +48,6 @@ export const PAGES = {
     "TrainingVideos": TrainingVideos,
     "WeatherAnalysis": WeatherAnalysis,
     "PanoramaGuideon": PanoramaGuideon,
-    "PanoramaGuide": PanoramaGuide,
 }
 
 export const pagesConfig = {
